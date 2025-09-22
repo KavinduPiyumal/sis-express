fb5a0af10355f06f10c5df1b6c9552d36bca35af
KavinduPiyumal <piyumal27100@gmail.com>
2025-09-23 02:39:10 +0530

    revert fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,55 +1,37 @@
-// require('dotenv').config();
-
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USER || 'postgres',
-//     password: process.env.DB_PASSWORD || 'password',
-//     database: process.env.DB_NAME || 'sis_database',
-//     host: process.env.DB_HOST || 'localhost',
-//     port: process.env.DB_PORT || 5432,
-//     dialect: 'postgres',
-//     logging: false,
-//   },
-//   test: {
-//     username: process.env.DB_USER || 'postgres',
-//     password: process.env.DB_PASSWORD || 'password',
-//     database: process.env.DB_NAME + '_test' || 'sis_database_test',
-//     host: process.env.DB_HOST || 'localhost',
-//     port: process.env.DB_PORT || 5432,
-//     dialect: 'postgres',
-//     logging: false,
-//   },
-//   production: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: 'postgres',
-//     logging: false,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   }
-// };
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'sis_database',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
   },
-  logging: false,
-});
-
-sequelize.authenticate()
-  .then(() => console.log('Database connected!'))
-  .catch(err => console.error('DB connection error:', err));
-
-
-module.exports = sequelize;
-
+  test: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME + '_test' || 'sis_database_test',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};

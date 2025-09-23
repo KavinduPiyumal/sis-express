d9371a90b633aca4eddb4e9de56d7a3d952364da
KavinduPiyumal <piyumal27100@gmail.com>
2025-09-23 23:29:22 +0530

    vercel, compatibility changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const pg = require("pg");
 
 module.exports = {
   development: {
@@ -8,15 +9,17 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
+    dialectModule: pg,
     logging: false,
   },
   test: {
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME + '_test' || 'sis_database_test',
+    database: (process.env.DB_NAME || 'sis_database') + '_test',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
+    dialectModule: pg,
     logging: false,
   },
   production: {
@@ -26,6 +29,7 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
+    dialectModule: pg,
     logging: false,
     dialectOptions: {
       ssl: {
